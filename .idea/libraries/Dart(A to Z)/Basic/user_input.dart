aaa155d9b597cf4ac309fcb46ea4f0623c153e5f
mdefa <mdefaj50@gmail.com>
2025-04-06 22:41:27 +0600

    Dart programming

--- .idea/libraries/Dart(A to Z)/Basic/user_input.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() {
-  print('Enter your name & Age');
-  String? name = stdin.readLineSync();
-  int ? age = int.tryParse(stdin.readLineSync()!);
-  print("My name is $name. I am $age old.");
-  print("My name is ${name?.toUpperCase()}. I am $age old.");
-}
